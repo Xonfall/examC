e68697d36060d196fecd0faf78863198be4d551b
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2017-06-14 14:41:02 +0200

    first part

--- exerciceC/main.c
+++ exerciceC/main.c
@@ -7,9 +7,33 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+
+
+
+void equalWords(char *wordOne, char *wordTwo) {
+    int compare = strcmp(wordOne, wordTwo);
+    
+    if (compare != 0) {
+        printf("Erreur : Mot différent !");
+    }
+}
+
+void rankWords(char *wordOne, char *wordTwo) {
+    int compare = strcmp(wordOne, wordTwo);
+    
+if (compare < 0) {
+        printf("Le mot %s est inférieur a %s", wordOne, wordTwo);
+    } else if (compare > 0){
+        printf("Le mot %s est inférieur a %s", wordTwo, wordOne);
+    } else {
+        printf("Les deux mots ont identiques ");
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    char str1[] = "bonjour";
+    char str2[] = "bonjour";
+    equalWords(str1, str2);
     return 0;
 }
