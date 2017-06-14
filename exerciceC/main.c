66e5e578f2a1bb7b055f3459f44606d3eee99043
Jérémy Bachayani <xonfall@MacBook-Pro-de-Jeremy.local>
2017-06-14 14:10:35 +0200

    Initial Commit

---
+++ exerciceC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  exerciceC
+//
+//  Created by Jérémy Bachayani on 14/06/2017.
+//  Copyright © 2017 Jérémy Bachayani. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
